2c41efd43a7ca41063ed6ab077ef418be7cd4855
deep7710 <34769411+deep7710@users.noreply.github.com>
2018-10-01 11:10:11 +0530

    Update server_startup.sh

--- server_startup.sh
+++ server_startup.sh
@@ -3,3 +3,9 @@
 yum install wget httpd mlocate java-1.8.0-openjdk-devel vim -y
 systemctl start httpd 
 yum install net-tools -y
+
+##install log shipper
+mkdir /home/filebeat
+cd /home/filebeat
+wget https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-5.0.0-x86_64.rpm
+rpm -ivh filebeat-5.0.0-x86_64.rpm
