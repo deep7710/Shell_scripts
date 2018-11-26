cc5224525f986ad9389aed7db7b50974317ebf50
deep7710 <34769411+deep7710@users.noreply.github.com>
2018-11-26 19:11:12 +0530

    script for installing prometheus

---
+++ install_prometheus.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+yum install vim wget java net-tools httpd telnet net-tools mlocate ntpd -y;service httpd start;ntpd -gq;service ntpd start
+wget https://github.com/prometheus/prometheus/releases/download/v2.5.0/prometheus-2.5.0.linux-amd64.tar.gz
+tar -xvf prometheus-2.5.0.linux-amd64.tar.gz
+mv prometheus-2.5.0.linux-amd64 prometheus
+mv prometheus /opt/
+touch /etc/systemd/system/prometheus.service
+sudo tee /etc/systemd/system/prometheus.service<<EOF
+[Unit]
+Description=Prometheus Server
+Documentation=https://prometheus.io/docs/introduction/overview/
+After=network-online.target
+[Service]
+User=root
+Restart=on-failure
+ExecStart=/opt/prometheus/prometheus --config.file=/opt/prometheus/prometheus.yml
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl daemon-reload
+service prometheus start
